f49270084218fd4c1b44d440b1cd25be792dc942
Amir Emam <prn.prn@yahoo.com>
2022-01-02 20:48:06 +0330

    Alaki Change

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
+class checkerboard{
+    public:
+    void initBoard(){//this is a function for initializing the board
+        for(int x = 1 ; x <= 8 ; x++){
+            for(int y = 1; y <= 8; y++){
+                if((y % 2) == 0){
+                    board[x][y] = '*';
+                }
+            }
+        }
+    } 
+    void printBoard(){
+
+    }
+    private:
+    char board[8][8];
+};
 int main(){
-//Officially Starting This Shit
+    checkerboard board;
+        board.initBoard(); 
+        board.printBoard(); 
 }
