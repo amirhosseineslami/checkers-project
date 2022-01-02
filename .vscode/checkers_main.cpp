95b3d13762b3d2c9d42e76d7de9e63456bb33b4f
Amir Hossein <a.h.eslami11@gmail.com>
2022-01-02 16:18:09 +0330

    a

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -9,7 +9,10 @@ cout<<"it is updated by amir hossein"<<endl;
 cout<<"it is updated again by him"<<endl;
 cout << "KHIKHIKHIKHIKHIKHIHKHI" << endl;
 cout << "CTRL + SHIFT + P" << endl;
+string opn;
     for(int i = 0; i < 7; i++ ){
-        cout << "Ridam Tu VScode" << endl;
+        cout << "Ridam Tu VScode" << opn << endl;
+        cin >> opn;
     }
+    
 }
