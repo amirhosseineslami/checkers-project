c016a59c3e422507fec077807d554869899e1b64
Amir Emam <prn.prn@yahoo.com>
2022-01-02 16:06:48 +0330

    ummm

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -7,4 +7,5 @@ int main(){
 cout<<"checkers testing code is running!"<<endl;
 cout<<"it is updated by amir hossein"<<endl;
 cout<<"it is updated again by him"<<endl;
+cout << "KHIKHIKHIKHIKHIKHIHKHI" << endl;
 }
