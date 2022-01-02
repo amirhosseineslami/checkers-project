f83455f2eb17c3bc40427d3ff86022776e32e6ec
Amir Emam <prn.prn@yahoo.com>
2022-01-02 21:12:07 +0330

    yekam taghir

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
 
 class checkerboard{
     public:
     void initBoard(){//this is a function for initializing the board
-        for(int x = 1 ; x <= 8 ; x++){
-            for(int y = 1; y <= 8; y++){
-                if((y % 2) == 0){
-                    board[x][y] = 'O';
+        cout << "  a   b   c   d   e   f   g   h" << endl;
+        cout << "+---+---+---+---+---+---+---+---+" << endl;
+        for(int x = 0 ; x < 8 ; x++){
+            for(int y = 0; y < 8; y++){
+                if(((x + y) % 2 != 0) && x <= 2){ 
+                    Oplayer[x][y] = 'O';
+                    cout << '|' << setw(2) << Oplayer[x][y] << setw(2) << '|'; 
                 }
-                else{
-                    board[x][y] = '*';
+                else if(x >= 5){
+                    starplayer[x][y] = '*';
+                    cout << '|' << setw(2) << starplayer[x][y] << setw(2) << '|'; 
                 }
             }
+            cout << endl;
         }
     } 
     void printBoard(){
@@ -21,9 +27,10 @@ class checkerboard{
     }
     private:
     char board[8][8];
+    char Oplayer[8][8];
+    char starplayer[8][8];
 };
 int main(){
     checkerboard board;
         board.initBoard(); 
-        board.printBoard(); 
 }
