6e46938143db64a4869799189e26c1facbddb278
Amir Emam <prn.prn@yahoo.com>
2022-01-02 20:49:38 +0330

    Update checkers_main.cpp

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -8,6 +8,9 @@ class checkerboard{
         for(int x = 1 ; x <= 8 ; x++){
             for(int y = 1; y <= 8; y++){
                 if((y % 2) == 0){
+                    board[x][y] = 'O';
+                }
+                else{
                     board[x][y] = '*';
                 }
             }
