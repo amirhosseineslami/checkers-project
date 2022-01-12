6f6eddf5e6b9c0fd0c336387c84533599f06f783
Amir Hossein <a.h.eslami11@gmail.com>
2022-01-12 13:41:28 +0330

    .

--- .vscode/checkers_main.cpp
+++ .vscode/checkers_main.cpp
@@ -2,10 +2,47 @@
 #include <iomanip>
 using namespace std;
 
+<<<<<<< HEAD
 class Cell{
 private:
 int x , y;
 bool isEmpty = true;
+=======
+class checkerboard{
+    public:
+    void initBoard(){//this is a function for initializing the board
+        cout << "  a   b   c   d   e   f   g   h" << endl;
+        cout << "+---+---+---+---+---+---+---+---+" << endl;
+        for(int x = 0 ; x < 8 ; x++){
+            for(int y = 0; y < 8; y++){
+                if (x <= 2 && (x + y) % 2 == 0 ){
+                    Oplayer[x][y] = ' ';
+                cout << '|' << setw(2) << Oplayer[x][y] <<  " ";    
+                }
+                else if(x <= 2 && (x + y) % 2 != 0){ 
+                    Oplayer[x][y] = 'O';
+                    cout << '|' << setw(2) << Oplayer[x][y] << " "; 
+
+                }
+                if(x > 2 && x < 5 ){
+                    cout << '|' << setw(4);
+                }
+                if(((x + y) % 2 != 0) && x >= 5){
+                    starplayer[x][y] = '*';
+                    cout << '|' << setw(2) << starplayer[x][y] <<  " "; 
+                }
+                else if(x >= 5 && (x + y) % 2 == 0){ 
+                    Oplayer[x][y] = ' ';
+                    cout << '|' << setw(2) << Oplayer[x][y] << " "; 
+
+                }
+            }
+            cout << "|" << setw(2) << x + 1 << endl;
+            cout << "+---+---+---+---+---+---+---+---+" << endl;
+        }
+    } 
+    void printBoard(){
+>>>>>>> parent of 2a9021f (Last Board)
 
 // ****** cell class ******
 public:
