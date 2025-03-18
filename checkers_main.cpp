c18ea6c92f9f1a79189e7359e9968c44d9271d61
Amir Hossein Eslami <95607919+amirhosseineslami@users.noreply.github.com>
2025-03-18 14:34:47 +0330

    Update checkers_main.cpp

--- checkers_main.cpp
+++ checkers_main.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 AmirHossein Eslami
+ * This project is licensed under the MIT License.
+ * See the LICENSE file for details.
+ */
 #include <iostream>
 #include <iomanip>
 #include <string>
